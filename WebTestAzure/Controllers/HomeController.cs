1dde3d921e66696e6b872430007da719fb0bbbc2
Anders Hundborg <anders.hundborg@outlook.dk>
2017-12-10 22:30:59 +0100

    First commit

--- WebTestAzure/Controllers/HomeController.cs
+++ WebTestAzure/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace WebTestAzure.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Hello Anders.";
 
             return View();
         }
